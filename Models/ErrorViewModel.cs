a3e47226bc6a98f52842dba82f3db20694eedb7d
Piotr Osowiec <osowiecp@zsl230.local>
2025-05-21 12:39:24 +0200

    dodanie plikow

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Katalog_gier.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
